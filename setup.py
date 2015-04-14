a1c1f9879d9382bedca5134ef3b963cdc0185076
avojnovicDk <ana.vojnovic@dobarkod.hr>
2015-04-14 17:24:43 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 exec(open("facebook/version.py").read())
 
 setup(
-    name='facebook-sdk',
+    name='facebook-sdka',
     version=__version__,
     description='This client library is designed to support the Facebook '
                 'Graph API and the official Facebook JavaScript SDK, which '
